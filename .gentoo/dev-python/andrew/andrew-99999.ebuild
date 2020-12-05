719d9755adde8180a9ced956809a5f49b001ac2e
klugh <klugh@student.ethz.ch>
2020-12-05 17:57:08 +0100

    first commit

---
+++ .gentoo/dev-python/andrew/andrew-99999.ebuild
@@ -0,0 +1,15 @@
+# Copyright 1999-2020 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+PYTHON_COMPAT=( python3_{6,7,8} )
+
+DESCRIPTION="Communication via telegram bot when bash command finishes executing"
+HOMEPAGE="https://github.com/Jimmy2027/ANDREW"
+
+LICENSE="GPL-3"
+SLOT="0"
+KEYWORDS=""
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(
+    name="ANDREW",
+    version="9999",
+    description="Communication via telegram bot when bash command finishes executing",
+    author="Hendrik Klug",
+    author_email="klugh@ethz.ch",
+    url="https://github.com/Jimmy2027/ANDREW",
+    keywords=["telegram-bot"],
+    scripts=['bin/andrew']
+)
