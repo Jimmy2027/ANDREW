223deda1a1a0dbccb2ce41d6ec5b32438b11b6f6
klugh <klugh@student.ethz.ch>
2020-12-05 19:04:31 +0100

    rename to norby

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 packages = find_packages()
 
 setup(
-    name="ANDREW",
+    name="NORBY",
     version="9999",
     description="Communication via telegram bot when bash command finishes executing",
     author="Hendrik Klug",
     author_email="klugh@ethz.ch",
     url="https://github.com/Jimmy2027/ANDREW",
     keywords=["telegram-bot"],
-    scripts=['bin/andrew']
+    scripts=['bin/norby']
 )
