191c17072a85419783121d37937534fb66611fff
klugh <klugh@student.ethz.ch>
2021-03-09 16:17:29 +0100

    added function get_config_path

--- src/norby/utils.py
+++ src/norby/utils.py
@@ -6,27 +6,31 @@ import configparser
 
 import requests
 
-# Reading the config
-CONFIG_PATH = Path('~/.config/norby_config.ini').expanduser()
-assert CONFIG_PATH.exists(), f'Config file not found under {CONFIG_PATH}. ' \
-                             f'Please fill the example under config and move it to {CONFIG_PATH}.'
+
+def get_config_path() -> Path:
+    """Get the config path."""
+
+    config_path = Path('~/.config/norby_config.ini').expanduser()
+    assert config_path.exists(), f'Config file not found under {config_path}. ' \
+                                 f'Please fill the example under config and move it to {config_path}.'
+
+    return config_path
 
 
 def get_config():
-    config_path = CONFIG_PATH
+    config_path = get_config_path()
     config = configparser.ConfigParser()
     config.read(config_path)
     return config
 
 
-def send_msg(message: str, config=None):
+def send_msg(message: str):
     """
     Send message to telegram chat bot.
 
     message str: text message to be sent to the chat bot.
     """
-    if not config:
-        config = get_config()
+    config = get_config()
     token = config['telegram_bot']['token']
     chat_id = config['telegram_bot']['chat_id']
     url_req = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'
