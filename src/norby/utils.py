f519ae588762726cd86e53f2358fdd8edcde1fba
klugh <klugh@student.ethz.ch>
2021-03-11 10:48:51 +0100

    added option to add loc_name to message

--- src/norby/utils.py
+++ src/norby/utils.py
@@ -18,21 +18,26 @@ def get_config_path() -> Path:
 
 
 def get_config():
+    """Read the config and return it as a configparser object."""
     config_path = get_config_path()
     config = configparser.ConfigParser()
     config.read(config_path)
     return config
 
 
-def send_msg(message: str):
+def send_msg(message: str, add_loc_name: bool = False):
     """
     Send message to telegram chat bot.
 
-    message str: text message to be sent to the chat bot.
+    message str: text message to be sent to the chat bot.$
+    add_loc_name: boolean indicating if the loc_name information should be added to the message. If true the string
+    "From {config["other"]["loc_name"]}" will be added at the beginning of the message.
     """
     config = get_config()
     token = config['telegram_bot']['token']
     chat_id = config['telegram_bot']['chat_id']
+    if add_loc_name:
+        message = f'From {config["other"]["loc_name"]}: ' + message
     url_req = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'
     result = requests.get(url_req)
     if result == '<Response [404]>':
